74b23a1609a8654148aae105d888c2c189bb7ed8
Raymond Mills <rmills.gabs@gmail.com>
2023-07-02 10:28:44 -0500

    added enemy with drop movement

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    [SerializeField]
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomX = Random.Range(-9f, 9f);
+        transform.Translate(Vector3.down * _speed * Time.deltaTime); 
+
+        if (transform.position.y < -4.4f)
+        {
+            transform.position = new Vector3(randomX, 4.4f, 0);
+        }
+    }
+}
