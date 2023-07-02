654c45a5bf5a5b2f225c183d7bd1154472a91a41
Raymond Mills <rmills.gabs@gmail.com>
2023-07-01 23:30:56 -0500

    added laser behavior

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,26 +7,36 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laseerPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-    transform.position = new Vector3(0, 0, 0);  
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        controlMovement();
+    }
+    void controlMovement()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+        Instantiate(_laseerPrefab, transform.position, Quaternion.identity);
+        }
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3 (horizontalInput, verticalInput, 0);
-        
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-       // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
 
-        transform.Translate (direction * _speed * Time.deltaTime);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
 
         if (transform.position.y >= 0)
         {
@@ -46,5 +56,7 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position = new Vector3(9f, transform.position.y, 0);
         }
+    }
 }
-}
+
+          
